21b273165d517e529c123ef1ab22287f9ef14eee
Francois Harbec <fharbec@gmail.com>
2016-02-17 13:46:13 -0800

    maybe this will work

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 
 module.exports = {
   name: 'stinkbomb',
-  included: function(app) {
-    app.import('vendor/stinkbomb.css');
+  included: function(app, parentAddon) {
+    var target = (parentAddon || app);
+
+    this._super.included.apply(this, arguments);
+    taget.import('vendor/stinkbomb.css');
   }
 };
